70971f129011d07b7ea145ea45fd7835beffa6cc
Long Xu <xulong@unishy.com>
2020-09-22 14:59:29 +0800

    Aded part headers, join body streams

--- src/multipart.lua
+++ src/multipart.lua
@@ -22,17 +22,20 @@ local function decode (chunk_size)
 
     local form, err = upload:new(chunk_size)
     if not form then
+        ngx.log(ngx.ERR, "failed to new upload: ", err)
         return nil -- There is no form data to read from
     end
 
     form:set_timeout(1000) -- 1 sec
 
+    local headers = {}
     local part_index = 1
     local part_name, part_value
 
     while true do
         local typ, res, err = form:read()
         if not typ then
+            ngx.log(ngx.ERR, "failed to read: ", err)
             return nil -- An error happened, 'failed to read'
         end
 
@@ -44,17 +47,33 @@ local function decode (chunk_size)
                     local current_value = stringy.strip(table.remove(current_parts, 1))
                     part_name = string.sub(current_value, 2, string.len(current_value) - 1)
                 end
+            else
+                ngx.log(ngx.DEBUG, "header part: ", res[3])
+                headers[res[1]] = res[2]
+                table.insert(headers, res[3])
             end
+
         elseif typ == "body" then
-            part_value = res
+            if part_value == nil then
+                part_value = res
+            else
+                part_value = part_value..res
+            end
+            ngx.log(ngx.DEBUG, "body size: ", #part_value)
         elseif typ == "part_end" then
             if part_name ~= nil then
-                result[part_name] = part_value
+                result[part_name] = {}
+                result[part_name].res = part_value
+                result[part_name].headers = {}
+                for k, v in pairs(headers) do
+                    result[part_name].headers[k] = v
+                end
 
                 -- Reset fields for the next part
                 part_value = nil
                 part_name = nil
                 part_index = part_index + 1
+                headers = {}
             end
         elseif typ == "eof" then
             break -- Finish reading the input
